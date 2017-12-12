662f8920cf1c84a8ed6e9ad8604666db3e0a37fa
Fabrice Andréïs <chwebdude@users.noreply.github.com>
2017-12-12 17:22:56 +0100

    Created view Transactions ad Initial Database

---
+++ CryptoManager/Models/DataModel/CryptoTransaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoManager.Models.DataModel
+{
+    public class CryptoTransaction
+    {
+        public Guid Id { get; set; }
+    }
+}
