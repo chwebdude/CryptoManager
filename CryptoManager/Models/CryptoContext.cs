cc68d8c433c959accaf17e1a85ac5376b58722c9
Fabrice Andréïs <fabrice.andreis@flowtec.endress.com>
2017-12-14 17:25:23 +0100

    Switched to Angular App

--- CryptoManager/Models/CryptoContext.cs
+++ CryptoManager/Models/CryptoContext.cs
@@ -9,6 +9,8 @@ namespace CryptoManager.Models
 {
     public class CryptoContext : DbContext
     {
+        public CryptoContext(DbContextOptions<CryptoContext> options) : base(options) { }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=crypto.db");
