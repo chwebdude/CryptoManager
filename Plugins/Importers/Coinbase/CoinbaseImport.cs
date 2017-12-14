4827b57102fd89b30d27d21af7ef114c6e8cb3bf
chwebdude <fabrice.andreis@gmail.com>
2017-12-14 23:18:04 +0100

    Get Coinbase buy transactions

--- Plugins/Importers/Coinbase/CoinbaseImport.cs
+++ Plugins/Importers/Coinbase/CoinbaseImport.cs
@@ -20,6 +20,7 @@ namespace Plugins.Importers.Coinbase
         {
             _apiKey = exchange.PublicKey;
             _apiSecret = exchange.PrivateKey;
+            var cryptoTransactions = new List<CryptoTransaction>();
 
             // 1. Get all Wallets
             var wallets = await ExecuteCoinbaseGet<IEnumerable<CoinbaseWallet>>("/v2/accounts");
@@ -28,12 +29,61 @@ namespace Plugins.Importers.Coinbase
             // 2. Query all Wallets
             foreach (var wallet in wallets)
             {
+                // 3. Query all Transactions
                 Logger.Debug("Get transactions of wallet {0}", wallet.Name);
                 var transactions = await ExecuteCoinbaseGet<IEnumerable<CoinbaseTransaction>>("/v2/accounts/" + wallet.Id + "/transactions?expand=all");
+                foreach (var transaction in transactions)
+                {
+                    var crypto = MappTransaction(transaction);
+                    cryptoTransactions.Add(crypto);
+                }
+                await Task.Delay(2000);
             }
 
+            return cryptoTransactions;
+        }
 
-            throw new NotImplementedException();
+        private CryptoTransaction MappTransaction(CoinbaseTransaction transaction)
+        {
+            var crypto = new CryptoTransaction
+            {
+                TransactionKey = transaction.Id,
+                DateTime = transaction.Created_At
+            };
+
+            switch (transaction.Type)
+            {
+                case CoinbaseTransactionTypes.Buy:
+                    crypto.BuyAmount = transaction.Buy.Amount.Amount;
+                    crypto.BuyCurrency = transaction.Buy.Amount.Currency;
+                    crypto.FeeAmount = transaction.Buy.Fee.Amount;
+                    crypto.FeeCurrency = transaction.Buy.Fee.Currency;
+                    break;
+
+                //case CoinbaseTransactionTypes.Sell:
+                //    crypto.SellAmount = transaction.Sell.Amount.Amount;
+                //    crypto.SellCurrency= transaction.Sell.Amount.Currency;
+                //    crypto.FeeAmount = transaction.Sell.Fee.Amount;
+                //    crypto.FeeCurrency = transaction.Buy.Fee.Currency;
+                //    break;
+
+                //case CoinbaseTransactionTypes.Transfer:
+                //    break;
+                //case CoinbaseTransactionTypes.Send:
+                //    break;
+                //case CoinbaseTransactionTypes.Fiat_Deposit:
+                //    break;
+                //case CoinbaseTransactionTypes.Fiat_Withdrawal:
+                //    break;
+                //case CoinbaseTransactionTypes.Exchange_Withdrawal:
+                //    break;
+                //case CoinbaseTransactionTypes.Exchange_Deposit:
+                //    break;
+                default:
+                    Logger.Error("Transaction Type not handled: {0}", transaction.Type);
+                    throw new ArgumentOutOfRangeException();
+            }
+            return crypto;
         }
 
         private async Task<T> ExecuteCoinbaseGet<T>(string requestPath)
