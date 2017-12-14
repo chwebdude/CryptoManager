e8cb6f77275b1ac8f338c62f47f1e1868c581e38
chwebdude <fabrice.andreis@gmail.com>
2017-12-14 22:19:45 +0100

    Transaction Types & details, Exchange structure

--- Model/DbModels/CryptoTransaction.cs
+++ Model/DbModels/CryptoTransaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using Model.Enums;
 
 namespace Model.DbModels
 {
@@ -7,5 +8,29 @@ namespace Model.DbModels
     {
         [Key]
         public Guid Id { get; set; }
+
+        public TransactionType Type { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public decimal InAmount { get; set; }
+        public string InCurrency { get; set; }
+
+        public decimal OutAmount { get; set; }
+        public string OutCurrency { get; set; }
+
+        
+        public decimal FeeAmount { get; set; }
+        public string FeeCurrency{ get; set; }
+
+        public decimal BuyAmount { get; set; }
+        public string BuyCurrency { get; set; }
+
+        public decimal SellAmount { get; set; }
+        public string SellCurrency { get; set; }
+
+
+        public Exchange Exchange { get; set; }
+        public string Comment { get; set; }
+        public string TransactionKey { get; set; }
     }
 }
---
+++ Model/Migrations/20171214211820_TransactionDetailAndExchange.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Model.Migrations
+{
+    public partial class TransactionDetailAndExchange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "BuyAmount",
+                table: "Transactions",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BuyCurrency",
+                table: "Transactions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "Transactions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateTime",
+                table: "Transactions",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ExchangeId",
+                table: "Transactions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "FeeAmount",
+                table: "Transactions",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FeeCurrency",
+                table: "Transactions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "InAmount",
+                table: "Transactions",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InCurrency",
+                table: "Transactions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "OutAmount",
+                table: "Transactions",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OutCurrency",
+                table: "Transactions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "SellAmount",
+                table: "Transactions",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SellCurrency",
+                table: "Transactions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TransactionKey",
+                table: "Transactions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Transactions",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Exchange",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    PrivateKey = table.Column<string>(nullable: true),
+                    PublicKey = table.Column<string>(nullable: true),
+                    SupportsPrivateKey = table.Column<bool>(nullable: false),
+                    SupportsPublicKey = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exchange", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_ExchangeId",
+                table: "Transactions",
+                column: "ExchangeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Exchange_ExchangeId",
+                table: "Transactions",
+                column: "ExchangeId",
+                principalTable: "Exchange",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Exchange_ExchangeId",
+                table: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Exchange");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_ExchangeId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "BuyAmount",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "BuyCurrency",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "DateTime",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "ExchangeId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "FeeAmount",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "FeeCurrency",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "InAmount",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "InCurrency",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "OutAmount",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "OutCurrency",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "SellAmount",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "SellCurrency",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionKey",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Transactions");
+        }
+    }
+}
