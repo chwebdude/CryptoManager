ca091bdd0b4604e67df5e00c2f217f3af99fdc82
chwebdude <chwebdude@users.noreply.github.com>
2017-12-15 10:06:40 +0100

    - Added Exchanges view - Disabled Server prerendering (better debugging experience)

---
+++ CryptoManager/ClientApp/app/components/exchanges/exchanges.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExchangesComponent } from './exchanges.component';
+
+describe('ExchangesComponent', () => {
+  let component: ExchangesComponent;
+  let fixture: ComponentFixture<ExchangesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ExchangesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExchangesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
