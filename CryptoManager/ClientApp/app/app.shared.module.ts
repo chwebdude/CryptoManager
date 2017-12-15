1dae46b61f6a96ce1a1a26d7aeee9be43ddfde77
Fabrice Andréïs <chwebdude@users.noreply.github.com>
2017-12-15 10:06:40 +0100

    - Added Exchanges view - Disabled Server prerendering (better debugging experience)

--- CryptoManager/ClientApp/app/app.shared.module.ts
+++ CryptoManager/ClientApp/app/app.shared.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { ExchangesComponent } from './components/exchanges/exchanges.component';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+      HomeComponent,
+      ExchangesComponent
     ],
     imports: [
         CommonModule,
@@ -27,6 +29,7 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'exchanges', component: ExchangesComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
