3421b8866779e0b41f30d9a3e3fb3c4d9cd3f6c5
Fabrice Andréïs <chwebdude@users.noreply.github.com>
2017-12-15 13:43:42 +0100

    Added plugin selector

---
+++ Model/Enums/Exchange.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Enums
+{
+    public enum Exchange
+    {
+        Coinbase = 1
+    }
+}
---
+++ Model/Meta/ExchangeMeta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Model.Enums;
+
+namespace Model.Meta
+{
+    public class ExchangeMeta
+    {
+        public Exchange ExchangeId { get; set; }
+        public string Name { get; set; }
+        public bool SupportsPublicKey => !string.IsNullOrEmpty(LabelPublicKey);
+        public string LabelPublicKey { get; set; }
+        public bool SupportsPrivateKey => !string.IsNullOrEmpty(LabelPrivateKey);
+        public string LabelPrivateKey { get; set; }
+    }
+}
