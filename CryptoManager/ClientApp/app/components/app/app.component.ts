aebac31b7b2e4c9560c45a506d4271485caf883f
Fabrice Andréïs <fabrice.andreis@flowtec.endress.com>
2017-12-15 12:21:01 +0100

    Added PrimeNg and Font-Awesome

--- CryptoManager/ClientApp/app/components/app/app.component.ts
+++ CryptoManager/ClientApp/app/components/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import '../../../../node_modules/primeng/resources/primeng.min.css';
+import '../../../../node_modules/primeng/resources/themes/omega/theme.css';
+import '../../../../node_modules/font-awesome/css/font-awesome.min.css';
+
+
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
