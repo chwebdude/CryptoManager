1debeff502cd8bfa620d03bd9f22f67d64258c23
chwebdude <fabrice.andreis@gmail.com>
2017-12-16 18:03:27 +0100

    Insert a exchange

--- Model/Migrations/20171216160014_AddImportExchanges.Designer.cs
+++ Model/Migrations/20171216160014_AddImportExchanges.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Model.Migrations
 {
     [DbContext(typeof(CryptoContext))]
-    partial class CryptoContextModelSnapshot : ModelSnapshot
+    [Migration("20171216160014_AddImportExchanges")]
+    partial class AddImportExchanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
