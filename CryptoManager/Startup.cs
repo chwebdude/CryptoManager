fb1dc6488e2545f0d63f9c6fb1b83b341b9accbf
chwebdude <fabrice.andreis@gmail.com>
2017-12-16 18:20:34 +0100

    Added Automapper

--- CryptoManager/Startup.cs
+++ CryptoManager/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using CryptoManager.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -40,6 +41,7 @@ namespace CryptoManager
                     Version = "v1"
                 }));
 
+            services.AddAutoMapper();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
