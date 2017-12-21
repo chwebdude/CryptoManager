8ab9a516aa2ad833b9bf3b20a28371d144e4d58c
chwebdude <fabrice.andreis@gmail.com>
2017-12-21 16:43:58 +0100

    Simplified investment calculation

--- Model/Migrations/20171221153849_SimplyfiedCreditcardBuys.Designer.cs
+++ Model/Migrations/20171221153849_SimplyfiedCreditcardBuys.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Model.Migrations
 {
     [DbContext(typeof(CryptoContext))]
-    partial class CryptoContextModelSnapshot : ModelSnapshot
+    [Migration("20171221153849_SimplyfiedCreditcardBuys")]
+    partial class SimplyfiedCreditcardBuys
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
