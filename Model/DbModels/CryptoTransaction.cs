77561f5aa51b9be6d208001b8ce3efb9f5305f64
chwebdude <fabrice.andreis@gmail.com>
2017-12-27 14:03:17 +0100

    Get Binance trades

--- Model/DbModels/CryptoTransaction.cs
+++ Model/DbModels/CryptoTransaction.cs
@@ -10,6 +10,21 @@ namespace Model.DbModels
         private CryptoTransaction() { }
 
         // Fee is always added!
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="transactionKey"></param>
+        /// <param name="dateTime"></param>
+        /// <param name="exchangeId"></param>
+        /// <param name="comment"></param>
+        /// <param name="buyAmount"></param>
+        /// <param name="buyCurrency"></param>
+        /// <param name="fee"></param>
+        /// <param name="feeCurrency"></param>
+        /// <param name="sellAmount">Without fee</param>
+        /// <param name="sellCurrency"></param>
+        /// <param name="usingWallet"></param>
+        /// <returns></returns>
         public static CryptoTransaction NewTrade(string transactionKey, DateTime dateTime, Guid exchangeId, string comment, decimal buyAmount, string buyCurrency, decimal fee, string feeCurrency, decimal sellAmount, string sellCurrency, bool usingWallet)
         {
             var t = new CryptoTransaction()
--- Plugins/Importers/Binance/BinanceImport.cs
+++ Plugins/Importers/Binance/BinanceImport.cs
@@ -2,16 +2,104 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Binance.Net;
+using Binance.Net.Objects;
 using Model.DbModels;
 using Model.Meta;
+using Newtonsoft.Json;
+using NLog;
 
 namespace Plugins.Importers.Binance
 {
-    class BinanceImport : IImporter
+    public class BinanceImport : IImporter
     {
-        public Task<IEnumerable<CryptoTransaction>> GetTransactions(Exchange exchange)
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        public async Task<IEnumerable<CryptoTransaction>> GetTransactions(Exchange exchange)
         {
-            throw new NotImplementedException();
+
+
+            var client = new BinanceClient(exchange.PublicKey, exchange.PrivateKey);
+            var trades = new List<CryptoTransaction>();
+
+            var prices = await client.GetAllPricesAsync();
+            var counter = 0;
+
+            foreach (var binancePrice in prices.Data)
+            {
+                var symbol = binancePrice.Symbol;
+                Logger.Debug("Ask " + symbol + " (" + counter++ + "/" + prices.Data.Length + ")");
+                string currency1 = null, currency2 = null;
+                var bccTrades = await client.GetMyTradesAsync(symbol);
+
+                if (symbol.EndsWith("BTC"))
+                {
+                    // Is BTC
+                    currency1 = "BTC";
+                    currency2 = symbol.Replace("BTC", "");
+                }
+
+                if (symbol.EndsWith("ETH"))
+                {
+                    // Is ETH
+                    currency1 = "ETH";
+                    currency2 = symbol.Replace("ETH", "");
+                }
+
+                if (symbol.EndsWith("BNB"))
+                {
+                    // Is BNB
+                    currency1 = "BNB";
+                    currency2 = symbol.Replace("BNB", "");
+                }
+
+                if (symbol.EndsWith("USDT"))
+                {
+                    // Is USDT
+                    currency1 = "USDT";
+                    currency2 = symbol.Replace("USDT", "");
+                }
+
+                if (currency1 == null || currency2 == null)
+                {
+                    if (symbol == "123456")
+                    {
+                        Logger.Warn("{0} symbol is not supported", symbol);
+                        continue;
+                    }
+
+                    Logger.Error("Unknown symbol: {0}. Aborting!", symbol);
+
+                    throw new ArgumentOutOfRangeException("Unknown symbol: " + symbol);
+                }
+
+                foreach (var trade in bccTrades.Data)
+                {
+                    if (trade.IsBuyer)
+                    {
+                        // Buy
+                        trades.Add(
+                            CryptoTransaction.NewTrade(trade.Id.ToString(), trade.Time, exchange.Id, "Binance Buy",
+                                trade.Quantity, currency2, trade.Commission, trade.CommissionAsset,
+                                trade.Price * trade.Quantity,
+                                currency1, true)
+                        );
+                    }
+                    else
+                    {
+                        // Sell
+                        trades.Add(
+                        CryptoTransaction.NewTrade(trade.Id.ToString(), trade.Time, exchange.Id, "Binance Sell",
+                            trade.Price * trade.Quantity, currency1, trade.Commission, trade.CommissionAsset, trade.Quantity,
+                            currency2, true)
+                            );
+                    }
+                }
+            }
+
+
+
+            return trades;
         }
 
         public ExchangeMeta GetExchangeMeta()
