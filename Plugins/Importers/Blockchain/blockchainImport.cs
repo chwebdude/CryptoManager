94fe015a8c73c21eeaff46175ac3f4669b4158a9
Fabrice Andréïs <fabrice.andreis@flowtec.endress.com>
2018-01-02 13:35:25 +0100

    A simple Bitcoin data fetcher

--- Plugins/Importers/Blockchain/blockchainImport.cs
+++ Plugins/Importers/Blockchain/blockchainImport.cs
@@ -1,35 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Info.Blockchain.API.BlockExplorer;
-using Info.Blockchain.API.Client;
 using Model.DbModels;
 using Model.Meta;
+using Newtonsoft.Json;
+using NLog;
+using RestSharp.Portable;
+using RestSharp.Portable.HttpClient;
 
 namespace Plugins.Importers.Blockchain
 {
-    public class BlockchainImport:IImporter
+    public class BlockchainImport : IImporter
     {
+        private static ILogger Logger = LogManager.GetCurrentClassLogger();
+
         public async Task<IEnumerable<CryptoTransaction>> GetTransactions(Exchange exchange)
         {
-            var api = new BlockExplorer();
-            var xpub = await api.GetXpub(exchange.PublicKey); 
-            foreach (var xpubTransaction in xpub.Transactions)
+            var list = new List<CryptoTransaction>();
+
+            var client = new RestClient("https://blockchain.info/");
+            var request = new RestRequest("multiaddr", Method.GET);
+            request.AddParameter("active", exchange.PublicKey);
+            Logger.Trace("GET blockchain.info for xPub");
+
+            var res = await client.Execute(request);
+            if (res.IsSuccess)
             {
-                //var allPrevious = xpubTransaction.Outputs.Sum(o => o..PreviousOutput.Value.Satoshis);
-                var allOut = xpubTransaction.Outputs.Sum(o => o.Value.Satoshis);
-
-                var previousOutput = xpubTransaction.Inputs.Sum(i => i.PreviousOutput.Value.Satoshis);
-
+                var response = JsonConvert.DeserializeObject<BlockchainResponse>(res.Content);
+                foreach (var transaction in response.Txs)
+                {
+                    if (transaction.Result > 0)
+                    {
+                        // Is Input
+                        list.Add(
+                            CryptoTransaction.NewIn(
+                               transaction.Hash,
+                                Helpers.UnixTimeStampToDateTime(transaction.Time),
+                                exchange.Id,
+                                "Receive",
+                                transaction.Result / 100000000,
+                                "BTC",
+                               transaction.Inputs.First().Prev_Out.Addr,
+                               transaction.Out.First(o => o.XPub.M == exchange.PublicKey).Addr,
+                                transaction.Hash
+                            ));
+                    }
+                    else
+                    {
+                        // Is Output
+                        var amount = (-1 * transaction.Result - transaction.Fee) / 100000000;
+                        var fee = transaction.Fee / 100000000;
+                        list.Add(
+                                                CryptoTransaction.NewOut(
+                                                    transaction.Hash,
+                                                    Helpers.UnixTimeStampToDateTime(transaction.Time),
+                                                    exchange.Id,
+                                                    "Sent",
+                                                    amount,
+                                                    "BTC",
+                                                    fee,
+                                                    "BTC",
+                                                    transaction.Inputs.First().Prev_Out.Addr,
+                                                    transaction.Out.First(o => o.XPub == null || o.XPub.M != exchange.PublicKey).Addr,
+                                                    transaction.Hash
+                                                ));
+                    }
+                }
             }
-            throw new NotImplementedException();
+            else
+            {
+                Logger.Error(res.StatusDescription);
+                Logger.Error(res.Content);
+                throw new Exception(res.StatusDescription);
+            }
+
+            return list;
+
         }
 
         public ExchangeMeta GetExchangeMeta()
         {
-            return new ExchangeMeta()
+            return new ExchangeMeta
             {
                 ExchangeId = Model.Enums.Exchange.Blockchain,
                 Name = "Blockchain.info",
