bd77dd20153ad9e729bb2add41e87b560df2781c
chwebdude <chwebdude@users.noreply.github.com>
2018-01-04 14:12:29 +0100

    Get historic data for transactions

--- Plugins/MarketData/CryptoCompare.cs
+++ Plugins/MarketData/CryptoCompare.cs
@@ -1,25 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using CryptoCompare;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Plugins.MarketData
 {
     public class CryptoCompare : IMarketData
-    {
+    {        
+
         #region Public Methods
 
         public async Task<decimal> GetCurrentRate(string baseCurrency, string currency)
         {
             var client = new CryptoCompareClient();
-            var x = await client.Prices.SingleAsync(currency, new[] {baseCurrency}, true);
+            var x = await client.Prices.SingleAsync(currency, new[] { baseCurrency }, true);
             return x.First().Value;
         }
+
 
         public async Task<decimal> GetHistoricRate(string baseCurrency, string currency, DateTime time)
         {
             var client = new CryptoCompareClient();
-            var x = await client.Prices.HistoricalAsync(currency, new[] {baseCurrency}, time);
+            var x = await client.Prices.HistoricalAsync(currency, new[] { baseCurrency }, time);
             return x.First().Value.First().Value;
         }
 
