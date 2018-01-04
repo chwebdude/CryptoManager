8d0532544b6516aa06b423aa7f79d54924000a16
chwebdude <chwebdude@users.noreply.github.com>
2018-01-04 12:28:57 +0100

    Get simple date from cryptocompare

---
+++ Plugins/MarketData/CryptoCompare.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CryptoCompare;
+
+namespace Plugins.MarketData
+{
+    public class CryptoCompare : IMarketData
+    {
+        public async Task<decimal> GetCurrentRate(string baseCurrency, string currency)
+        {
+            var client = new CryptoCompareClient();
+            var x = await client.Prices.SingleAsync(currency, new[] { baseCurrency }, true);
+            return x.First().Value;
+        }
+
+        public async Task<decimal> GetHistoricRate(string baseCurrency, string currency, DateTime time)
+        {
+            var client = new CryptoCompareClient();
+            var x = await client.Prices.HistoricalAsync(currency, new[] { baseCurrency }, time);
+            return x.First().Value.First().Value;
+        }
+    }
+}
