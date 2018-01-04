f3b7c04aeba72ebba0dcbc37ad132de66c3ab454
Fabrice Andréïs <chwebdude@users.noreply.github.com>
2018-01-04 14:31:37 +0100

    Added caching for current rate

--- Plugins/MarketData/CryptoCompare.cs
+++ Plugins/MarketData/CryptoCompare.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using CryptoCompare;
 using Microsoft.Extensions.Caching.Memory;
@@ -9,22 +7,31 @@ using Microsoft.Extensions.Caching.Memory;
 namespace Plugins.MarketData
 {
     public class CryptoCompare : IMarketData
-    {        
+    {
+        #region Static Fields and Constants
+
+        private static readonly MemoryCache Cache = new MemoryCache(new MemoryCacheOptions());
+
+        #endregion
 
         #region Public Methods
 
         public async Task<decimal> GetCurrentRate(string baseCurrency, string currency)
         {
-            var client = new CryptoCompareClient();
-            var x = await client.Prices.SingleAsync(currency, new[] { baseCurrency }, true);
-            return x.First().Value;
+            return await Cache.GetOrCreate(baseCurrency + currency, async entry =>
+            {
+                var client = new CryptoCompareClient();
+                var x = await client.Prices.SingleAsync(currency, new[] {baseCurrency}, true);
+                var res = x.First().Value;
+                return res;
+            });
         }
 
 
         public async Task<decimal> GetHistoricRate(string baseCurrency, string currency, DateTime time)
         {
             var client = new CryptoCompareClient();
-            var x = await client.Prices.HistoricalAsync(currency, new[] { baseCurrency }, time);
+            var x = await client.Prices.HistoricalAsync(currency, new[] {baseCurrency}, time);
             return x.First().Value.First().Value;
         }
 
