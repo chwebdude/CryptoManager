d6da1e16ff1f4c2583d61b1f6c2039c41d47564a
Fabrice Andréïs <chwebdude@users.noreply.github.com>
2018-01-04 12:06:33 +0100

    Added Marketdata interface

---
+++ Plugins/IMarketData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Plugins
+{
+    public interface IMarketData
+    {
+        decimal GetCurrentRate(string baseCurrency, string toCurrency);
+        decimal GetHistoricRate(string baseCurrency, string toCurrency, DateTime time);
+    }
+}
