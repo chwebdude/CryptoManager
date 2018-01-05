53f9584f0fb9b9b63671183bc1ef4df7103ad94d
Fabrice Andréïs <chwebdude@users.noreply.github.com>
2018-01-05 10:57:18 +0100

    Better error logging and handling. Try 3 times if data can't be fetched

--- Plugins/MarketData/CryptoCompare.cs
+++ Plugins/MarketData/CryptoCompare.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CryptoCompare;
 using Microsoft.Extensions.Caching.Memory;
+using NLog;
 
 namespace Plugins.MarketData
 {
@@ -11,6 +12,7 @@ namespace Plugins.MarketData
         #region Static Fields and Constants
 
         private static readonly MemoryCache Cache = new MemoryCache(new MemoryCacheOptions());
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
 
         #endregion
 
@@ -21,9 +23,28 @@ namespace Plugins.MarketData
             return await Cache.GetOrCreate(baseCurrency + currency, async entry =>
             {
                 var client = new CryptoCompareClient();
-                var x = await client.Prices.SingleAsync(currency, new[] {baseCurrency}, true);
-                var res = x.First().Value;
-                return res;
+                Exception ex = null;
+
+                // Try 3 times, or return 0
+                for (int i = 0; i < 3; i++)
+                {
+                    try
+                    {
+
+
+                        var x = await client.Prices.SingleAsync(currency, new[] { baseCurrency }, true);
+                        var res = x.First().Value;
+                        return res;
+                    }
+                    catch (Exception e)
+                    {
+                        Logger.Warn(e);
+                        ex = e;
+                        await Task.Delay(5000);
+                    }
+                }
+                Logger.Error(ex, "Failted to get Historic Market data");
+                return 0;
             });
         }
 
@@ -31,8 +52,27 @@ namespace Plugins.MarketData
         public async Task<decimal> GetHistoricRate(string baseCurrency, string currency, DateTime time)
         {
             var client = new CryptoCompareClient();
-            var x = await client.Prices.HistoricalAsync(currency, new[] {baseCurrency}, time);
-            return x.First().Value.First().Value;
+            Exception ex = null;
+
+            // Try 3 times, or return a error
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    var x = await client.Prices.HistoricalAsync(currency, new[] { baseCurrency }, time);
+
+                    return x.First().Value.First().Value;
+
+                }
+                catch (Exception e)
+                {
+                    Logger.Warn(e);
+                    ex = e;
+                    await Task.Delay(5000);
+
+                }
+            }
+            throw new Exception("Failted to get Historic Market data", ex);
         }
 
         #endregion
