99abfbe2c1c2527d13e6219da873df209453f723
Fabrice Andréïs <chwebdude@users.noreply.github.com>
2018-01-08 11:54:51 +0100

    Get Coin info

--- Plugins/IMarketData.cs
+++ Plugins/IMarketData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Model.Meta;
 
 namespace Plugins
 {
@@ -9,5 +10,6 @@ namespace Plugins
         Task<decimal> GetCurrentRate(string baseCurrency, string currency);
         //Task<decimal> GetCurrentRate(string baseCurrency, IEnumerable<string> currencies);
         Task<decimal> GetHistoricRate(string baseCurrency, string currency, DateTime time);
+        Task<CoinMeta> GetCoinInfo(string symbol);
     }
 }
