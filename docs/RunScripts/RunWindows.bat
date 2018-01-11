418c5b2959eb7909944e0372dc243d5adda15a19
Fabrice Andréïs <fabrice.andreis@flowtec.endress.com>
2018-01-11 16:08:57 +0100

    New Startup batch for windows

--- docs/RunScripts/RunWindows.bat
+++ docs/RunScripts/RunWindows.bat
@@ -1,3 +1,13 @@
-start  "bin\CryptoManager.exe"
-timeout /t 10
+@echo off
+
+IF EXIST data (
+ SET wait=2
+) ELSE (
+ SET wait=10
+ ECHO Firststart: Wait till initialization...
+)
+
+cd bin
+start CryptoManager.exe
+timeout /t %Wait%
 start http://localhost:5000
