dedf5c05af8cc8fc017bbe9df7a8d510579141ca
chwebdude <chwebdude@users.noreply.github.com>
2018-02-05 15:12:21 +0100

    Bity get history

--- Plugins/Importers/Bity/BityImport.cs
+++ Plugins/Importers/Bity/BityImport.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Model.DbModels;
 using Model.Meta;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Plugins.MarketData;
+using RestSharp.Portable;
+using RestSharp.Portable.HttpClient;
 
 namespace Plugins.Importers.Bity
 {
@@ -17,8 +23,43 @@ namespace Plugins.Importers.Bity
             _marketData = marketData;
         }
 
-        public Task<IEnumerable<CryptoTransaction>> GetTransactions(Exchange exchange)
+        public async Task<IEnumerable<CryptoTransaction>> GetTransactions(Exchange exchange)
         {
+            var client = new RestClient("https://bity.com/");
+
+
+            var authRequest = new RestRequest("o/token/", Method.POST);
+            authRequest.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            authRequest.AddHeader("Accept", "application/json, text/plain, */*");
+            authRequest.AddParameter("client_id", "QmaTkYI50XmCF18fupZgdAOptYqDzVix12RpqFYS");
+            authRequest.AddParameter("grant_type", "password");
+            authRequest.AddParameter("username", exchange.PublicKey);
+            authRequest.AddParameter("password", exchange.Passphrase);
+
+            var authResp = await client.Execute(authRequest);
+            var token = JsonConvert.DeserializeObject<BityAuthResponse>(authResp.Content);
+            
+
+            var historyRequest = new RestRequest("/api/v1/order/?limit=100&offset=0&order_by=-timestamp_created", Method.GET);
+            historyRequest.AddHeader("Authorization", token.Token_Type + " " + token.Access_Token);
+            var historyResponse = await client.Execute(historyRequest);
+
+
+            var data = JsonConvert.DeserializeObject<Orders>(historyResponse.Content);
+            var executed = data.Objects.Where(o => o.Status == "EXEC");
+
+            foreach (var trade in executed)
+            {
+                if (trade.Category == "SELL")
+                {
+
+                }
+                else
+                {
+                    
+                }
+            }
+
             throw new NotImplementedException();
         }
 
