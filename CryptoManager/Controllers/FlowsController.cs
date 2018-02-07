492f15ec6a7877a312d47d6b64d2aa69148f7e4f
Fabrice Andréïs <fabrice.andreis@flowtec.endress.com>
2018-02-07 17:14:37 +0100

    Add Input and Output Nodes

---
+++ CryptoManager/Controllers/FlowsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CryptoManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.DbModels;
+
+namespace CryptoManager.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Flows")]
+    public class FlowsController : Controller
+    {
+        private readonly CryptoContext _context;
+
+        public FlowsController(CryptoContext context)
+        {
+            _context = context;
+        }
+
+        // Todo: Filter by Exchange
+        [HttpGet]
+        public async Task<IEnumerable<FlowNode>> GetNodes()
+        {
+            return await Task.Run(() => _context.FlowNodes);            
+        }
+    }
+}
---
+++ Model/Migrations/20180207154831_NewFlowStructure.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Model.Migrations
+{
+    public partial class NewFlowStructure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flows");
+
+            migrationBuilder.CreateTable(
+                name: "FlowLinks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Amount = table.Column<decimal>(nullable: false),
+                    Currency = table.Column<string>(nullable: true),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    FlowNodeSource = table.Column<Guid>(nullable: false),
+                    FlowNodeTarget = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlowLinks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FlowNodes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Amount = table.Column<decimal>(nullable: false),
+                    Comment = table.Column<string>(nullable: true),
+                    Currency = table.Column<string>(nullable: true),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    ExchangeId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlowNodes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FlowLinks");
+
+            migrationBuilder.DropTable(
+                name: "FlowNodes");
+
+            migrationBuilder.CreateTable(
+                name: "Flows",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Amount = table.Column<decimal>(nullable: false),
+                    Currency = table.Column<string>(nullable: true),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    ExchangeId = table.Column<Guid>(nullable: false),
+                    FlowId = table.Column<Guid>(nullable: true),
+                    TransactionId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flows", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Flows_Flows_FlowId",
+                        column: x => x.FlowId,
+                        principalTable: "Flows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flows_FlowId",
+                table: "Flows",
+                column: "FlowId");
+        }
+    }
+}
