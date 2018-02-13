cfbeb35aff0338902a0660af394817613e8a4ae1
chwebdude <chwebdude@users.noreply.github.com>
2018-02-13 11:27:17 +0100

    BTC Naming override

--- Plugins/Importers/Kraken/KrakenImport.cs
+++ Plugins/Importers/Kraken/KrakenImport.cs
@@ -69,6 +69,9 @@ namespace Plugins.Importers.Kraken
             {
                 assetsCache.Add(assetInfo.Key, assetInfo.Value.Altname);
             }
+
+            // Overrides
+            assetsCache["XXBT"] = "BTC";
         }
 
         public ExchangeMeta GetExchangeMeta() => new ExchangeMeta()
